6699918800df367b2931ac193bc12023016db2ab
zhangjian <zhangxjian@me.com>
2023-01-12 17:18:05 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,29 @@
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 
-export default {
-  input: './src/index.ts',
-  output: {
-    dir: './dist',
-    entryFileNames: '[name].js',
-    sourcemap: true,
-    format: 'cjs',
+export default [
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        dir: './build',
+        entryFileNames: '[name].cjs.js',
+        sourcemap: true,
+        format: 'cjs',
+      },
+      {
+        dir: './build',
+        entryFileNames: '[name].js',
+        sourcemap: true,
+        format: 'iife',
+        name: 'RandomPasswordGenerator',
+      },
+    ],
+    plugins: [
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+      }),
+    ],
   },
-  plugins: [
-    commonjs(),
-    typescript({
-      tsconfig: './tsconfig.json',
-    }),
-  ],
-};
+];
