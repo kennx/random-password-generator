9ae14cfb41d70b00d757a4488eb8af858e176a3b
zhangjian <zhangxjian@me.com>
2023-01-12 15:36:57 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ Array.prototype.shuttle = function (): Array<any> {
 String.prototype.shuttle = function (): string {
   const _arr = this.split('');
   if (_arr.length > 0 && !!_arr.join('').trim()) {
-    const _sortArr = _arr.sort(() => Math.random() - Math.random());
+    const _sortArr = _arr.shuttle();
     return _sortArr.join('');
   }
   return _arr.join();
@@ -47,19 +47,55 @@ class PasswordGenerator {
     }
   }
 
-  public generate() {
+  public generate(): string {
     this.setCharacterSets();
+    this.setCharacterLength();
+    const result = this.handleCharacterSets();
+    return result;
+  }
+
+  private handleCharacterSets(): string {
+    let result = '';
+    let _str = '';
+    let _first = '';
+    const _sets = this.characterSets.filter((set) => set.active);
+    _sets.map((set) => {
+      if (!set.first) {
+        const _characters = set.characters.shuttle().substring(0, set.size!);
+        result += _characters;
+      } else {
+        _str = set.characters.shuttle().substring(0, set.size!);
+        if (set.size && set.size > 1) {
+          _first = _str.substring(0, 1);
+          _str = _str.substring(1, _str.length);
+        } else {
+          _first = _str;
+          _str = '';
+        }
+      }
+    });
+    _str = _str.shuttle();
+    result += _str;
+    result = _first + result.shuttle();
+    return result;
   }
 
   private setCharacterLength() {
     const _sets = this.characterSets.filter((set) => set.active);
     const _setsSize = _sets.length;
     const _length = this.config.length;
-    const _avg = Math.floor(_length / _setsSize);
-    _sets.map(set => set.size = _avg);
+    const _average = Math.floor(_length / _setsSize);
+    const _resto = _length % _setsSize;
+    _sets.map((set) => (set.size = _average));
+    if (!!_resto) {
+      for (let i = _resto; i > 0; i--) {
+        _sets[i].size! += 1;
+      }
+    }
   }
 
   private setCharacterSets() {
+    this.characterSets = [];
     this.characterSets.push({
       name: 'uppper',
       characters: UPPER_CHARACTERS,
@@ -88,5 +124,13 @@ class PasswordGenerator {
 }
 
 const rpg = new PasswordGenerator();
+
+rpg.config.length = 17;
+rpg.config.first = FIRST_CHARACTERS.UPPER;
+console.log(rpg.generate());
+rpg.config.first = FIRST_CHARACTERS.SYMBOL;
+rpg.config.length = 7;
+rpg.config.numeric = false;
+console.log(rpg.generate());
 
 export default PasswordGenerator;
